2855515d1925834e936e1a13ac5bb125b41afc16
lasse <77295879+lassejlv@users.noreply.github.com>
2024-04-04 11:24:02 +0200

    Add minutter og timer til bus tider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// Call the renderData function to display data
+renderData();
+
+fetchAllData();
+
 async function fetchData(url) {
     try {
         const response = await fetch(url);
@@ -47,7 +52,6 @@ async function renderData() {
     if (!data) {
         return;
     }
-    
 
     data.forEach(item => {
         const card = document.createElement('div');
@@ -64,8 +68,3 @@ async function renderData() {
         container.appendChild(card);
     });
 }
-
-// Call the renderData function to display data
-renderData();
-
-fetchAllData();
